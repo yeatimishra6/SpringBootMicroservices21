79d2f3e5a56021dac3ee8ecb770e658526ee7e43
yeatimishra6 <yeatimishra6@gmail.com>
2021-09-09 14:40:28 -0400

    Update /endpoint

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -3,11 +3,27 @@ package com.plantplaces;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Handel the /Start Endpoint
+ * @return
+ */
+
 @Controller
 public class PlantPlacesController {
 
 	@RequestMapping("/start")
 	public String start() 
+	{
+		
+		return "start";
+	}
+	/**
+	 * Handle the / Endpoint
+	 * @return
+	 */
+
+	@RequestMapping("/start")
+	public String index() 
 	{
 		
 		return "start";
